0903939525f3205c69df588e676ff53f7579b0f3
YuriAR <yrreis@gmail.com>
2017-01-25 11:44:16 -0300

    First commit all

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+    @FXML
+    Label processId;
+    @FXML
+    Label processStatus;
+    @FXML
+    Button buttonRequest;
+    @FXML
+    Button buttonRelease;
+}
