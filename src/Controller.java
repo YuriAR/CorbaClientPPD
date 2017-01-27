dbf9f966000005d3b9262778db5c29bb26789e74
YuriAR <yrreis@gmail.com>
2017-01-27 13:12:31 -0300

    cliente pronto pra testes

--- src/Controller.java
+++ src/Controller.java
@@ -4,6 +4,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContext;
+import org.omg.CosNaming.NamingContextHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,16 +27,38 @@ public class Controller implements Initializable, UICommunication{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        buttonRelease.setDisable(true);
-        ProcessInterfaceImpl processInterface = new ProcessInterfaceImpl(this);
-        //alert pedindo porta
+        //TODO: alert pedindo porta
 
-            buttonRequest.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        buttonRelease.setDisable(true);
+        final CorbaManager manager = new CorbaManager("1");
+        manager.initCorba(new String[] {"localhost", "1515"});
+
+        try{
+            ORB orb = ORB.init(new String[] {"localhost", "1515"},null);
+            org.omg.CORBA.Object objPoa = orb.resolve_initial_references("RootPOA");
+            POA rootPOA = POAHelper.narrow(objPoa);
+            org.omg.CORBA.Object obj = orb.resolve_initial_references("NameService");
+            NamingContext naming = NamingContextHelper.narrow(obj);
+            ProcessInterfaceImpl processInterface = new ProcessInterfaceImpl(this);
+            org.omg.CORBA.Object   objRef =	 rootPOA.servant_to_reference(processInterface);
+            NameComponent[] name = {new NameComponent("Processo" + manager.myPid,"Exemplo")};
+            naming.rebind(name,objRef);
+            rootPOA.the_POAManager().activate();
+            processStatus.setText("Processo pronto");
+            orb.run();
+
+        }catch (Exception ex){
+            System.out.println("Erro");
+            ex.printStackTrace();
+        }
+
+        buttonRequest.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
                     //Enviar request
                     buttonRequest.setDisable(true);
                     processStatus.setText("Aguardando recurso");
+                    manager.requestResource();
                 }
             });
 
@@ -39,6 +68,7 @@ public class Controller implements Initializable, UICommunication{
                     //Enviar release
                     buttonRelease.setDisable(true);
                     processStatus.setText("IDLE");
+                    manager.releaseResource();
                 }
             });
 
--- src/CorbaManager.java
+++ src/CorbaManager.java
@@ -1,3 +1,5 @@
+import CoordinatorResourceCommunication.CoordinatorInterface;
+import CoordinatorResourceCommunication.CoordinatorInterfaceHelper;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContext;
@@ -7,6 +9,15 @@ import org.omg.CosNaming.NamingContextHelper;
  * Created by yurir on 26/01/2017.
  */
 public class CorbaManager {
+
+    CoordinatorInterface coordinator;
+    String myPid;
+
+
+    public CorbaManager(String pId){
+        this.myPid = pId;
+    }
+
     public void initCorba(String [] args){
         try{
 
@@ -15,26 +26,24 @@ public class CorbaManager {
 
             NamingContext naming = NamingContextHelper.narrow(obj);
 
-             NameComponent[] name = {new NameComponent("Calculadora","Exemplo")};
+            NameComponent[] name = {new NameComponent("Coordinator","Exemplo")};
 
             org.omg.CORBA.Object objRef =  naming.resolve(name);
 
-//            Calculadora calc = CalculadoraHelper.narrow(objRef);
-//
-//            try{
-//
-//                System.out.println("5+3="+ calc.soma(5,3));
-//                System.out.println("5/0="+ calc.divisao(5,0));
-//
-//            }catch (DivisaoPorZero ex){
-//                System.out.println("Divisao Por Zero");
-//                System.out.println("A Diviao foi "+ex.arg1 +"/ "+ex.arg2);
-//            }
+            coordinator = CoordinatorInterfaceHelper.narrow(objRef);
 
         }catch (Exception e) {
             System.out.println("Outro Erro : " + e);
             e.printStackTrace(System.out);
         }
+    }
+
+    public void requestResource(){
+        coordinator.requestResource(myPid);
+    }
+
+    public void releaseResource(){
+        coordinator.releaseResource();
     }
 
 }
